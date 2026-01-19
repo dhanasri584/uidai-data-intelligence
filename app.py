ca350c12c99cfb47d0df3a5d1d6a6e1132c4f10c
dhanasri584 <dhanasrikanisetti@gmail.com>
2026-01-20 01:01:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,10 +55,10 @@ state_fix = {
     "andaman and nicobar islands": "Andaman & Nicobar Islands",
     "orissa": "Odisha",
     "odissa": "Odisha",
-    "dadra & nagar haveli and daman and diu": "Dadra & Nagar Haveli ",
-    "dadra & nagar haveli": "Dadra & Nagar Haveli ",
+    "dadra & nagar haveli ": "Dadra & Nagar Haveli ",
+   
     "daman & diu": "Daman & Diu",
-    "dadra and nagar haveli": "Dadra & Nagar Haveli And Daman & Diu",
+    "dadra and nagar haveli": "Dadra & Nagar Haveli",
     "dadra and nagar haveli ": "Dadra & Nagar Haveli ",
     "pondicherry":"puducherry",
     "jammu & kashmir":"jammu and kashmir" 
